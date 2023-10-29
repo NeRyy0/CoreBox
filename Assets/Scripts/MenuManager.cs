4e4226b7a628563569c5e2a43af70a456569ce34
NeRyy0 <145838366+NeRyy0@users.noreply.github.com>
2023-10-29 15:13:32 +0700

    1.3
    -Добавлен Post Processing

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,18 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] public GameObject exitConfirmation;
+    [SerializeField] TMP_Text nickName;
 
-    public void Play()
+    [SerializeField] public GameObject exitConfirmation;
+    [SerializeField] public GameObject mapChoise;
+
+    private void Start()
+    {
+        PlayerPrefs.GetString("nickName");
+    }
+
+    public void PlayLegacy()
     {
         SceneManager.LoadScene(1);
     }
 
+    public void PlayCoreWood()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void PlayPlains()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void MapChoise()
+    {
+        mapChoise.SetActive(true);
+    }
+
     public void ExitConfirmation()
     {
+        PlayerPrefs.SetString("nickName", nickName.ToString());
         exitConfirmation.SetActive(true);
     }
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -16,6 +18,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Animator animator;
 
     bool isGrounded = true;
+
+    [SerializeField] AudioSource runSound;
+
+    [SerializeField] AudioClip jumpSound;
+
+    //[SerializeField] Image RightUpSight;
+    //[SerializeField] Image RightDownSight;
+    //[SerializeField] Image LeftUpSight;
+    //[SerializeField] Image LeftDownSight;
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +48,8 @@ public class PlayerController : MonoBehaviour
         if (direction.x != 0 || direction.z != 0)
         {
             animator.SetBool("Walk", true);
+            //RightUpSight.transform.Translate(Vector2.right * 4 * Time.deltaTime);
+            //RightUpSight.transform = Mathf.Clamp(60, 40, 0);
         }
         if (direction.x == 0 && direction.z == 0)
         {
@@ -49,10 +62,17 @@ public class PlayerController : MonoBehaviour
             {
                 stamina -= Time.deltaTime;
                 currentSpeed = shiftSpeed;
+                animator.SetBool("Run", true);
+
+                if (!runSound.isPlaying && isGrounded)
+                {
+                    runSound.Play();
+                }
             }
             else
             {
                 currentSpeed = movementSpeed;
+                animator.SetBool("Run", false);
             }
         }
 
@@ -60,6 +80,9 @@ public class PlayerController : MonoBehaviour
         {
             stamina += Time.deltaTime;
             currentSpeed = movementSpeed;
+            animator.SetBool("Run", false);
+
+            runSound.Stop();
         }
 
         if (stamina > 5f)
@@ -75,6 +98,17 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
             isGrounded = false;
+            runSound.Stop();
+            AudioSource.PlayClipAtPoint(jumpSound, transform.position);
+        }
+
+        if (!isGrounded)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
         }
     }
 
